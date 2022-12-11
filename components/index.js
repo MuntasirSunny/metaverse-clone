0bcad8c18a86f155e020ed31f5fa7f875cc0451b
MuntasirSunny <hossainmdmuntasir@gmail.com>
2022-12-11 20:53:17 +0600

    eslint-error-check

--- components/index.js
+++ components/index.js
@@ -1,10 +1,10 @@
-import Navbar from './Navbar';
-import { TypingText, TitleText } from './CustomTexts';
+import { TitleText, TypingText } from './CustomTexts';
 import ExploreCard from './ExploreCard';
-import StartSteps from './StartSteps';
-import NewFeatures from './NewFeatures';
-import InsightCard from './InsightCard';
 import Footer from './Footer';
+import InsightCard from './InsightCard';
+import Navbar from './Navbar';
+import NewFeatures from './NewFeatures';
+import StartSteps from './StartSteps';
 
 export {
   Navbar,
